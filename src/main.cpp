1f665194455e4e0108c13bf8e6caf4725ac8821a
milestorm <milestorm@milestorm.net>
2017-06-25 12:25:54 +0200

    Added display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+#include <TM1637Display.h>
 
+// Display Module connection pins (Digital Pins)
+#define CLK 11
+#define DIO 10
 
 int needToPrint = 0;
 int count;
@@ -9,15 +13,28 @@ int trueState = LOW;
 unsigned long lastStateChangeTime = 0;
 int cleared = 0;
 
+int cursorPosition = 0;
+String alarmTimeTemp = "";
+String alarmTime = "";
+
 // constants
 
 unsigned int dialHasFinishedRotatingAfterMs = 100;
 unsigned int debounceDelay = 10;
 
+uint8_t clearData[] = { 0x00, 0x00, 0x00, 0x00 };
+TM1637Display display(CLK, DIO);
+
+
 void setup(){
+
 
   Serial.begin(9600);
   pinMode(dialerInput, INPUT_PULLUP);
+
+	// clear display
+	display.setBrightness(0x0f);
+	display.setSegments(clearData);
 
 }
 
@@ -30,7 +47,22 @@ void loop(){
     if (needToPrint) {
       // if it's only just finished being dialed, we need to send the number down the serial
       // line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
-      Serial.print(count % 10, DEC);
+			if (cursorPosition == 0) {
+				display.setSegments(clearData);
+			}
+			count = count % 10;
+			//Serial.print(count, DEC);
+			display.showNumberDec(count, false, 1, cursorPosition);
+			alarmTimeTemp += count;
+			cursorPosition++;
+
+			if (cursorPosition > 3) {
+				alarmTime = alarmTimeTemp;
+				Serial.println(alarmTime);
+				alarmTimeTemp = "";
+				cursorPosition = 0;
+			}
+
       needToPrint = 0;
       count = 0;
       cleared = 0;
